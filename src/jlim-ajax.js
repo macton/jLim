19882b55efcbf8cc581d5fccc890ba793a4f4e95
Victor Villaverde Laan <info@freelancephp.net>
2012-04-08 16:47:05 +0200

    Script depends on SimpleAjax (for better maintenance) and added module pattern

--- src/jlim-ajax.js
+++ src/jlim-ajax.js
@@ -10,7 +10,9 @@
  * @author Victor Villaverde Laan
  * @link http://www.freelancephp.net/jlim-ajax-plugin/
  */
-jLim.fn.extend(
+(function ($, SimpleAjax) {
+
+$.fn.extend(
 	/**
 	 * @lends jLim.fn
 	 */
@@ -25,7 +27,7 @@ jLim.fn.extend(
 		load: function (url, data, complete) {
 			var self = this;
 
-			jLim.ajax({
+			$.ajax({
 				url: url,
 				type: data ? 'POST' : 'GET',
 				data: data || null,
@@ -40,48 +42,24 @@ jLim.fn.extend(
 	}
 );
 
-jLim.extend(
+$.extend(
 	/**
 	 * @lends jLim
 	 */
 	{
-		/**
-		* Make querystring outof object or array of values
-		* @param {Object|Array} obj Keys/values
-		* @return {String} The querystring
-		*/
-		param: function (obj) {
-			var s = [];
-
-			jLim.each( obj, function (k, v) {
-				s.push(encodeURIComponent(k) +'='+ encodeURIComponent(v));
-			});
-
-			return s.join('&');
-		},
 
 		/**
 		* Default ajax settings
 		* @property {Object} ajaxSettings
 		*/
-		ajaxSettings: {
-			url: '',
-			type: 'GET',
-			async: true,
-			cache: true,
-			data: null,
-			contentType: 'application/x-www-form-urlencoded',
-			success: null,
-			error: null,
-			complete: null
-		},
+		ajaxSettings: SimpleAjax.settings,
 
 		/**
 		* Change the default ajax settings
 		* @param {Object} settings Overwrite the default settings, see ajaxSettings
 		*/
 		ajaxSetup: function (settings) {
-			jLim.extend(jLim.ajaxSettings, settings);
+			$.extend($.ajaxSettings, settings);
 		},
 
 		/**
@@ -90,48 +68,8 @@ jLim.extend(
 		* @return {XMLHttpRequest|ActiveXObject}
 		*/
 		ajax: function (options) {
-			var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'),
-				opts = jLim.extend({}, jLim.ajaxSettings, options),
-				ready = function () {
-					if (xhr.readyState == 4) {
-						if (xhr.status == 200) {
-							if (jLim.isFunction(opts.success))
-								opts.success.call(opts, xhr.responseText, xhr.status, xhr);
-						} else {
-							if (jLim.isFunction(opts.error))
-								opts.error.call(opts, xhr, xhr.status);
-						}
-
-						if (jLim.isFunction(opts.complete))
-							opts.complete.call(opts, xhr, xhr.status);
-					}
-				};
-
-			if (!opts.cache)
-				opts.url += ((opts.url.indexOf('?') > -1) ? '&' : '?') + '_nocache='+ (new Date()).getTime();
-
-			if ( opts.data ) {
-				if (opts.type == 'GET') {
-					opts.url += ((opts.url.indexOf('?') > -1) ? '&' : '?') + jLim.param(opts.data);
-					opts.data = null;
-				} else {
-					opts.data = jLim.param(opts.data);
-				}
-			}
-
-			// set request
-			xhr.open(opts.type, opts.url, opts.async);
-			xhr.setRequestHeader('Content-type', opts.contentType);
-
-			if (opts.async) {
-				xhr.onreadystatechange = ready;
-				xhr.send(opts.data);
-			} else {
-				xhr.send(opts.data);
-				ready();
-			}
-
-			return xhr;
+			SimpleAjax.call(options);
+			return SimpleAjax.xhr;
 		},
 
 		/**
@@ -142,12 +80,12 @@ jLim.extend(
 		* @return {XMLHttpRequest|ActiveXObject}
 		*/
 		get: function (url, data, success) {
-			if (jLim.isFunction(data)) {
+			if ($.isFunction(data)) {
 				success = data;
 				data = null;
 			}
 
-			return jLim.ajax({
+			return $.ajax({
 				url: url,
 				type: 'GET',
 				data: data,
@@ -163,12 +101,12 @@ jLim.extend(
 		* @return {XMLHttpRequest|ActiveXObject}
 		*/
 		post: function (url, data, success) {
-			if (jLim.isFunction(data)) {
+			if ($.isFunction(data)) {
 				success = data;
 				data = null;
 			}
 
-			return jLim.ajax({
+			return $.ajax({
 				url: url,
 				type: 'POST',
 				data: data,
@@ -177,3 +115,6 @@ jLim.extend(
 		}
 	}
 );
+
+
+})(jLim, SimpleAjax);
