a9cb5bb6bbed9642afdfdf181218f66c92f64a18
Victor Villaverde Laan <info@freelancephp.net>
2012-04-13 14:09:38 +0200

    Improved encapsulation of globals and some code layout changes

--- src/jlim-event.js
+++ src/jlim-event.js
@@ -3,9 +3,9 @@
  *
  * @fileOverview
  *    Event handling methods for jLim, supporting:
- *    - Using custom events, f.e. $().bind( 'customEvent', func );
+ *    - Using custom events, f.e. $().bind('customEvent', func);
  *    - Using event delegation
- *    - Using namespace in event names ( only one level, f.e. "click:form" )
+ *    - Using namespace in event names (only one level, f.e. "click:form")
  *    - quick methods for default event types, like .click(), .blur() etc.
  *    Released under MIT license.
  * @package jLim
@@ -13,151 +13,7 @@
  * @author Victor Villaverde Laan
  * @link http://www.freelancephp.net/jlim-event-plugin/
  */
-(function ($) {
-
-/**
- * Prepare event object for cross-browser compatibility
- * @private
- * @param {EventObject} evt
- * @return {EventObject} Made cross-browser compatible
- */
-function getEvent(evt) {
-	if (!evt)
-		evt = window.event || {};
-
-	// Copy event object
-	var e = $.extend({}, evt);
-
-	// Reference to original event object
-	e.originalEvent = evt;
-
-	// The source element
-	e.target = evt.target || evt.srcElement;
-
-	// keyCode, charCode, which
-	if (evt.charCode) {
-		e.keyCode = evt.charCode;
-		e.which = evt.charCode;
-	} else if (evt.keyCode) {
-		e.charCode = evt.keyCode;
-		e.which = evt.keyCode;
-	} else if (evt.which) {
-		e.keyCode = evt.which;
-		e.charCode = evt.which;
-	}
-
-	// Prevent triggering the default action of the event
-	e.preventDefault = function () {
-		// IE
-		evt.returnValue = false;
-
-		// Standard
-		if (evt.preventDefault)
-			evt.preventDefault();
-
-		e.isDefaultPrevented = true;
-	};
-	e.isDefaultPrevented = false;
-
-	// Prevent event bubbling
-	e.stopPropagation = function () {
-		// IE
-		evt.cancelBubble = true;
-
-		// Standard
-		if ( evt.stopPropagation )
-			evt.stopPropagation();
-
-		e.isPropagationStopped = true;
-	};
-	e.isPropagationStopped = false;
-
-	return e;
-};
-
-/**
- * Get or set event handlers
- * @private
- * @param {DOMElement} target
- * @param {String} eventName
- * @param {Function} [fn] Set function as event handler
- * @param {Number} [insertIndex] Insert function at given index
- */
-function eventHandlers(target, eventName, fn, insertIndex) {
-	var name = $.trim((eventName || '').replace(':', '.')),
-		eName = name.substr(0, name.indexOf('.') > -1 ? name.indexOf('.') : name.length),
-		nameSpace = name.substr(eName.length + 1) || '__default',
-		handlers = target.__jLimEventHandlers || {},
-		wrapFunc = function (event) {
-			if ($.isFunction(curFunc))
-				curFunc.call(target);
-
-			triggerEvent(target, eName, [getEvent(event)]);
-		},
-		curFunc;
-
-	// getter
-	if (!fn) {
-		// check eventHandlers property
-		if (!handlers || !handlers[eName])
-			return null;
-
-		return $.trim(eventName) == eName || nameSpace == '__default' ? handlers[eName] : handlers[eName][nameSpace];
-	}
-
-	// init handlers prop not exists
-	if (!handlers)
-		handlers = {};
-
-	// set wrapper function on first use of target
-	if (!handlers[eName]) {
-		curFunc = target['on'+ eName];
-		target['on'+ eName] = wrapFunc;
-	}
-
-	// init event when not exists
-	if (!handlers[eName])
-		handlers[eName] = {};
-
-	// init namespace when not exists
-	if (!handlers[eName][nameSpace])
-		handlers[eName][nameSpace] = [];
-
-	// add handler function
-	if (typeof insertIndex == 'undefined') {
-		handlers[eName][nameSpace].push(fn);
-	} else {
-		handlers[eName][nameSpace].splice(insertIndex, 0, fn);
-	}
-
-	// set prop
-	target.__jLimEventHandlers = handlers;
-}
-
-/**
- * Trigger event function
- * @private
- * @param {DOMElement} target
- * @param {String} eventName
- * @param {Array} args
- */
-function triggerEvent(target, eventName, args) {
-	var handlers = eventHandlers(target, eventName),
-		fns;
-
-	// loop through handlers
-	for(var k in handlers) {
-		fns = handlers[k];
-
-		if ($.isArray(fns)) {
-			for(var i in fns) {
-				fns[i].apply(target, args);
-			}
-		} else {
-			fns.apply(target, args);
-		}
-	}
-};
+(function (window, $) {
 
 $.fn.extend(
 	/**
@@ -293,4 +149,148 @@ $.each(['load', 'unload', 'scroll', 'resize', 'error', 'blur', 'change', 'focus'
 	};
 });
 
-})(jLim);
+/**
+ * Prepare event object for cross-browser compatibility
+ * @private
+ * @param {EventObject} evt
+ * @return {EventObject} Made cross-browser compatible
+ */
+function getEvent(evt) {
+	if (!evt)
+		evt = window.event || {};
+
+	// Copy event object
+	var e = $.extend({}, evt);
+
+	// Reference to original event object
+	e.originalEvent = evt;
+
+	// The source element
+	e.target = evt.target || evt.srcElement;
+
+	// keyCode, charCode, which
+	if (evt.charCode) {
+		e.keyCode = evt.charCode;
+		e.which = evt.charCode;
+	} else if (evt.keyCode) {
+		e.charCode = evt.keyCode;
+		e.which = evt.keyCode;
+	} else if (evt.which) {
+		e.keyCode = evt.which;
+		e.charCode = evt.which;
+	}
+
+	// Prevent triggering the default action of the event
+	e.preventDefault = function () {
+		// IE
+		evt.returnValue = false;
+
+		// Standard
+		if (evt.preventDefault)
+			evt.preventDefault();
+
+		e.isDefaultPrevented = true;
+	};
+	e.isDefaultPrevented = false;
+
+	// Prevent event bubbling
+	e.stopPropagation = function () {
+		// IE
+		evt.cancelBubble = true;
+
+		// Standard
+		if (evt.stopPropagation)
+			evt.stopPropagation();
+
+		e.isPropagationStopped = true;
+	};
+	e.isPropagationStopped = false;
+
+	return e;
+};
+
+/**
+ * Get or set event handlers
+ * @private
+ * @param {DOMElement} target
+ * @param {String} eventName
+ * @param {Function} [fn] Set function as event handler
+ * @param {Number} [insertIndex] Insert function at given index
+ */
+function eventHandlers(target, eventName, fn, insertIndex) {
+	var name = $.trim((eventName || '').replace(':', '.')),
+		eName = name.substr(0, name.indexOf('.') > -1 ? name.indexOf('.') : name.length),
+		nameSpace = name.substr(eName.length + 1) || '__default',
+		handlers = target.__jLimEventHandlers || {},
+		wrapFunc = function (event) {
+			if ($.isFunction(curFunc))
+				curFunc.call(target);
+
+			triggerEvent(target, eName, [getEvent(event)]);
+		},
+		curFunc;
+
+	// getter
+	if (!fn) {
+		// check eventHandlers property
+		if (!handlers || !handlers[eName])
+			return null;
+
+		return $.trim(eventName) == eName || nameSpace == '__default' ? handlers[eName] : handlers[eName][nameSpace];
+	}
+
+	// init handlers prop not exists
+	if (!handlers)
+		handlers = {};
+
+	// set wrapper function on first use of target
+	if (!handlers[eName]) {
+		curFunc = target['on'+ eName];
+		target['on'+ eName] = wrapFunc;
+	}
+
+	// init event when not exists
+	if (!handlers[eName])
+		handlers[eName] = {};
+
+	// init namespace when not exists
+	if (!handlers[eName][nameSpace])
+		handlers[eName][nameSpace] = [];
+
+	// add handler function
+	if (typeof insertIndex == 'undefined') {
+		handlers[eName][nameSpace].push(fn);
+	} else {
+		handlers[eName][nameSpace].splice(insertIndex, 0, fn);
+	}
+
+	// set prop
+	target.__jLimEventHandlers = handlers;
+}
+
+/**
+ * Trigger event function
+ * @private
+ * @param {DOMElement} target
+ * @param {String} eventName
+ * @param {Array} args
+ */
+function triggerEvent(target, eventName, args) {
+	var handlers = eventHandlers(target, eventName),
+		fns;
+
+	// loop through handlers
+	for (var k in handlers) {
+		fns = handlers[k];
+
+		if ($.isArray(fns)) {
+			for(var i in fns) {
+				fns[i].apply(target, args);
+			}
+		} else {
+			fns.apply(target, args);
+		}
+	}
+};
+
+})(window, jLim);
