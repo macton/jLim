898ff3e5a5376a0d4d9da438079ec13532d21026
Victor Villaverde Laan <info@freelancephp.net>
2012-04-13 14:13:20 +0200

    CSS refactoring and added css() method

--- src/jlim-css.js
+++ src/jlim-css.js
@@ -17,43 +17,90 @@ $.fn.extend(
 	 */
 	{
 		/**
-		* Add class to selected elements
-		* @param {String} value
-		* @return {This}
-		*/
+		 * Add class to selected elements
+		 * @param {String} value
+		 * @return {This}
+		 */
 		addClass: function (value) {
 			return this.each(function () {
 				if (!$(this).hasClass(value))
-					this.className += ' '+ value;
+					this.className = this.className.length > 0 ? this.className +' '+ value : value;
 			});
 		},
 
 		/**
-		* Remove class from selected elements
-		* @param {String} value
-		* @return {This}
-		*/
+		 * Remove class from selected elements
+		 * @param {String} value
+		 * @return {This}
+		 */
 		removeClass: function (value) {
+			var exp = new RegExp('(\\s|^)' + value + '(\\s|$)');
+
 			return this.each(function () {
-				this.className = this.className.replace(new RegExp('(\\s|^)' + value + '(\\s|$)'), '');
+				var className = this.className.replace(exp, ' ');
+				this.className = $.trim(className);
 			});
 		},
 
 		/**
-		* Check if first element has given class
-		* @param {String} value
-		* @return {Boolean}
-		*/
+		 * Check if first element has given class
+		 * @param {String} value
+		 * @return {Boolean}
+		 */
 		hasClass: function (value) {
-			return (this.length && this.get(0).className.match(new RegExp( '(\\s|^)' + value + '(\\s|$)')));
+			return value && this.length && this.get(0).className.match(new RegExp( '(\\s|^)' + value + '(\\s|$)'));
 		},
 
 		/**
-		* Set style(s) of the style attribute
-		* @param {String|Object} style
-		* @param {Mixed} [value] Set new value
-		* @return {This|String}
-		*/
+		 * Set or get css value
+		 * @param {String|Object} cssProperty
+		 * @param {Mixed} [value] Set new value
+		 * @return {This|String}
+		 */
+		css: function (cssProperty, value) {
+			var d = window.document,
+				key;
+
+			// getter
+			if (typeof value == 'undefined') {
+				if (typeof cssProperty == 'string') {
+					// getter
+					if (this.length) {
+						if (d.defaultView && d.defaultView.getComputedStyle) {
+							key = cssProperty.toLowerCase();
+							return d.defaultView.getComputedStyle(this.get(0), '').getPropertyValue(key);
+						} else if (this.get(0).currentStyle) {
+							key = $.toCamelCase(cssProperty);
+							return this.get(0).currentStyle[key];
+						}
+					}
+
+					return null;
+				} else {
+					// setter multiple styles
+					for (key in cssProperty)
+						this.css(key, cssProperty[key]);
+
+					return this;
+				}
+			}
+
+			// setter
+			return this.each(function () {
+				if (!this.style.cssText)
+					this.style.cssText +=  ';';
+
+				this.style.cssText += cssProperty + ':' + value;
+			});
+		},
+
+		/**
+		 * Set or get style(s) of the style attribute
+		 * @deprecated
+		 * @param {String|Object} style
+		 * @param {Mixed} [value] Set new value
+		 * @return {This|String}
+		 */
 		style: function (style, value) {
 			var key;
 
@@ -89,11 +136,11 @@ $.extend(
 	 */
 	{
 		/**
-		* Convert dashed names to CamelCase
-		* F.e. border-bottom-width to borderBottomWidth
-		* @param {String} str
-		* @return {String} CamelCased
-		*/
+		 * Convert dashed names to CamelCase
+		 * F.e. border-bottom-width to borderBottomWidth
+		 * @param {String} str
+		 * @return {String} CamelCased
+		 */
 		toCamelCase: function(str) {
 			return str.replace(/(\-[a-z])/g, function($1) {
 				return $1.toUpperCase().replace('-', '');
